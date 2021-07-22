4066a992f956e2d5bb053c070e13ba2244a40cfb
winkymonkey <pal.abhishek.2008@gmail.com>
2021-07-22 11:46:44 +0530

    refactor

--- src/main/java/org/example/spring/boot/MyRestController.java
+++ src/main/java/org/example/spring/boot/MyRestController.java
@@ -1,30 +1,28 @@
-package com.example.spring.boot;
+package org.example.spring.boot;
 
 import java.util.Arrays;
 import java.util.List;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 
-@Path("/v1/main")
-@Produces(MediaType.APPLICATION_JSON)
-public class MyJerseyResource {
+@RestController
+@RequestMapping("/v1/main")
+public class MyRestController {
 	
 	@Autowired
 	private RestTemplate restTemplate;
 	
 	private static final String REMOTE_URL = "http://localhost:8091/remoteApp/person";
 	
-	private static final Logger LOGGER = LogManager.getLogger(MyJerseyResource.class);
+	private static final Logger LOGGER = LogManager.getLogger(MyRestController.class);
 	
 	
 	/**
@@ -32,14 +30,13 @@ public class MyJerseyResource {
 	 * URL: http://localhost:8080/v1/main/endpoint0
 	 * ----------------------------------------------------
 	 */
-	@GET
-	@Path("/endpoint0")
-	public String endpoint0() {
+	@GetMapping("/endpoint0")
+	public List<Person> endpoint0() {
 		LOGGER.info("entering endpoint0");
 		ResponseEntity<Person[]> response = restTemplate.getForEntity(REMOTE_URL, Person[].class);
 		List<Person> personList = Arrays.asList(response.getBody());
 		personList.forEach(person -> LOGGER.info("person="+person));
-		return "Success String0";
+		return personList;
 	}
 	
 }
